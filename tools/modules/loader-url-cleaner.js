e24f298e480fe4dae9083c0a9b9c4b3bc5fe460c
Sherif Salama <80130495+sherow1982@users.noreply.github.com>
2025-11-03 12:19:27 +0200

    تحميل وحدة url-cleaner.js في الصفحة الأصلية

---
+++ tools/modules/loader-url-cleaner.js
@@ -0,0 +1,6 @@
+(function(){
+  const s = document.createElement('script');
+  s.src = 'https://sherow1982.github.io/arabic-ads-calculator-tools/tools/modules/url-cleaner.js';
+  s.defer = true;
+  document.head.appendChild(s);
+})();
