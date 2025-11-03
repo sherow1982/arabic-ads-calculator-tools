971dac47e49d29f942796545e95271c0bcd3b63a
Sherif Salama <80130495+sherow1982@users.noreply.github.com>
2025-11-03 11:42:26 +0200

    تحميل وحدة باك لينك DA عالي في الصفحة الأصلية

---
+++ tools/modules/loader-backlink.js
@@ -0,0 +1,7 @@
+// Include backlink booster module
+(function(){
+  const s = document.createElement('script');
+  s.src = 'https://sherow1982.github.io/arabic-ads-calculator-tools/tools/modules/backlink-booster.js';
+  s.defer = true;
+  document.head.appendChild(s);
+})();
