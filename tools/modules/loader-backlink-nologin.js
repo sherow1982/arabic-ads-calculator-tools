ca73790031cb40499baa85219a3536f8a836050d
Sherif Salama <80130495+sherow1982@users.noreply.github.com>
2025-11-03 12:54:15 +0200

    تحميل وحدة backlink-nologin في الصفحة

---
+++ tools/modules/loader-backlink-nologin.js
@@ -0,0 +1,5 @@
+(function(){
+  const s=document.createElement('script');
+  s.src='https://sherow1982.github.io/arabic-ads-calculator-tools/tools/modules/backlink-nologin.js';
+  s.defer=true; document.head.appendChild(s);
+})();
