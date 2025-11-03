51d0af507b19e347bb3c4fee005004f16665ec60
Sherif Salama <80130495+sherow1982@users.noreply.github.com>
2025-11-03 13:24:03 +0200

    تحميل وحدات الباك لينك مع تعطيل الكاش وإتاحة retryBlocked

--- tools/modules/loader-backlink-nologin.js
+++ tools/modules/loader-backlink-nologin.js
@@ -1,5 +1,4 @@
 (function(){
-  const s=document.createElement('script');
-  s.src='https://sherow1982.github.io/arabic-ads-calculator-tools/tools/modules/backlink-nologin.js';
-  s.defer=true; document.head.appendChild(s);
+  const s1=document.createElement('script'); s1.src='https://sherow1982.github.io/arabic-ads-calculator-tools/tools/modules/backlink-nologin.js?v='+Date.now(); s1.defer=true; document.head.appendChild(s1);
+  const s2=document.createElement('script'); s2.src='https://sherow1982.github.io/arabic-ads-calculator-tools/tools/modules/backlink-retry.js?v='+Date.now(); s2.defer=true; document.head.appendChild(s2);
 })();
